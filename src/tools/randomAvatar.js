b07e1ca3b55d2775eeda02d02f159913951b63cd
VentCat <73477803+dsxksss@users.noreply.github.com>
2023-06-03 16:29:03 +0800

    删除多余文件

--- src/tools/randomAvatar.js
+++
@@ -1,6 +0,0 @@
-function randomAvatar(){
-    const randomNum = Math.floor(Math.random() * 5) + 1;;
-    console.log(randomNum);
-    return `https://daisyui.com/tailwind-css-component-profile-${randomNum}@56w.png`;    
-}
-export default randomAvatar;
